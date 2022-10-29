e02d6df78f86cdd2e16c6d47e2055f2cc9d283ac
Shiirookami <rzkir818@gmail.com>
2022-10-29 09:32:56 +0700

    Install adminlte

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+});
+// ->name('home')->middleware('auth');
