15765faf160bc889fb5f2f4a36992c30bd978b5e
Shiirookami <rzkir818@gmail.com>
2022-12-15 17:42:42 +0700

    Datatable activated

--- config/adminlte.php
+++ config/adminlte.php
@@ -364,7 +364,7 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
--- resources/views/print_books.blade.php
+++ resources/views/print_books.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 <body>
     <h1 class="text-center">Data Buku</h1>
